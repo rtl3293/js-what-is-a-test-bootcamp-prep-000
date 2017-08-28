fcbd9227f61ff2494fd60b671e3a23c15fae6c4d
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-08-28 03:46:51 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,4 +1,4 @@
-/*var index = require("../index.js")
+var index = require("../index.js")
 
 
 describe('what-is-a-test', () => {
@@ -22,4 +22,4 @@ describe('what-is-a-test', () => {
     })
   })
 })
-*/
+
